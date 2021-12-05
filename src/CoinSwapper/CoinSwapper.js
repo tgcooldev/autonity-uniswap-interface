f2165e6659c25064a85c9e19299b83b3035b7801
tgcooldev <76828588+tgcooldev@users.noreply.github.com>
2021-12-05 14:28:49 +0100

    Update CoinSwapper.js

--- src/CoinSwapper/CoinSwapper.js
+++ src/CoinSwapper/CoinSwapper.js
@@ -164,7 +164,7 @@ function CoinSwapper(props) {
     // We only update the values if the user provides a token
     else if (address) {
       // Getting some token data is async, so we need to wait for the data to return, hence the promise
-      getBalanceAndSymbol(account, address, provider, signer, weth.address, coins).then((data) => {
+      getBalanceAndSymbol(account, address, provider, signer, '0xE1DBa9e8b071240Fab022fDf70Aea6Fd74C5233b', coins).then((data) => {
         setCoin1({
           address: address,
           symbol: data.symbol,
